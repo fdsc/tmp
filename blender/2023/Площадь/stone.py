2b68d73140118eb8a7ae393be52b3892a7caec14
fdsc <fdsc@yandex.ru>
2023-01-06 13:42:11 +0300

    automatic commit

--- blender/2023/Площадь/stone.py
+++ blender/2023/Площадь/stone.py
@@ -18,15 +18,17 @@ Samples   = 1024
 Prefilter = 'ACCURATE'
 CountOfCarbage = 186*2
 use_denoising = True
+SegmentsOfCurves = 64
 
 SimpleRendering = True
 
 if SimpleRendering:
-	Samples   = 64
-	Prefilter = 'FAST'
-	# Prefilter = 'NONE'
-	CountOfCarbage = 64
-	use_denoising  = False
+    Samples   = 64
+    Prefilter = 'FAST'
+    # Prefilter = 'NONE'
+    CountOfCarbage = 64
+    use_denoising  = False
+    SegmentsOfCurves = 16
 
 
 blend_dir = os.path.dirname("/Arcs/Repos/tmp/blender/2023/Площадь/")
@@ -42,7 +44,7 @@ MatProps = MaterialProperties.MaterialProperties()
 MatProps.DeleteAllObjects()
 
 
-StonesCollectionName = 'Камень'
+StonesCollectionName = 'Камни'
 # Перед созданием коллекции убираем выделение с объектов, чтобы они не добавились по ошибке
 bpy.ops.object.select_all(action='DESELECT')
 StonesCollection = bpy.data.collections.new(StonesCollectionName)
@@ -56,38 +58,70 @@ view_layer        = bpy.context.view_layer
 
 
 # Создаём плоскость, параллельную плоскости XY
-def newVert():
-	
-	vertices = []
+def СоздатьТочкиКамня():
 
-	# Масштабный коэффициент
-	k = 1
+    vertices = []
 
-	vertices.append([-1.0, -1.0, -1.0]);
-    vertices.append([+1.0, -1.0, -1.0]);
-    vertices.append([-1.0, -1.0, +1.0]);
-    vertices.append([+1.0, -1.0, +1.0]);
+    # Масштабный коэффициент
+    k = 1
     
-	return vertices
+    def создатьЗаготовкуЭллипса(r1, r2, cnt):
+        points = []
+        
+        signs = [-1, +1]
+        step  = 2*pi / cnt
 
-def newEdjes():
+        # Уравнение эллипса
+        # x = a cos α
+        # y = b sin α
+        # 1 = x^2/a^2 + y^2/b^2
+        #for s1 in signs:
+            # for s2 in signs:
+        #    points.append([s1*r1,  0*r2, 0]);
+        #    points.append([ 0*r1, s1*r2, 0]);
+            
+        #    points.append([ 0*r1, s1*r2, 0]);
+        for i in range(cnt):
+            points.append([r1 * cos(step*i),  r2 * sin(step*i), 0])
+            
+        edges = []
+        cnt   = 0
+        for p in points:
+            if cnt > 0:
+                edges.append((cnt-1, cnt));
+            cnt += 1
 
-	faces = [
-				(0, 1, 2, 3)
-			]
+        edges.append((cnt-1, 0));
 
-	edjes = []
+        return [points, edges]
+    
+    # Создаём псевдо-эллипс
+    def getPseudoEllipse(center, r1, r2):
+        None
 
-	eds = [0, 1, 2, 3]
-	for i in range(  len(eds)-1  ):
-		edjes.append((  eds[i]+0, eds[i+1]+0  ))
+    [points, eds] = создатьЗаготовкуЭллипса(1, 2, SegmentsOfCurves)
+    cnt    = 0
+    cnte   = 0
+    face   = ()
+    faces  = []
+    edges  = []
+    for p in points:
+        vertices.append(p);
+        face = face + (cnt,)
+        cnt += 1
 
-	return faces, edjes
+    for ie in range(len(eds)):
+        e = eds[ie]
+        edges.append((e[0] + cnte, e[1] + cnte));
+    cnte += len(eds)
+
+    # faces.append(face)
+
+    return (vertices, faces, edges)
 
 
 def СоздатьКамень():
-    vertices       = newVert()
-    [faces, edjes] = newEdjes()
+    [vertices, faces, edjes] = СоздатьТочкиКамня()
 
     name = 'Камень'
     [object, mesh] = MatProps.createObject(
@@ -95,6 +129,28 @@ def СоздатьКамень():
         )
 
     
+СоздатьКамень()
 
 
 
+
+
+for obj in bpy.data.objects:
+	obj.display_type = 'SOLID'
+    # obj.display_type = 'TEXTURED'
+
+# Добавляем настройки рендера
+bpy.data.scenes[0].render.engine = 'CYCLES'
+bpy.data.scenes[0].cycles.preview_samples = 16
+bpy.data.scenes[0].cycles.samples = Samples
+# bpy.data.scenes[0].cycles.seed = 0
+bpy.data.scenes[0].cycles.denoising_prefilter = Prefilter
+bpy.data.scenes[0].cycles.use_denoising = use_denoising
+# Эта штука позволяет сделать сцену более освещённой при рендеринге
+bpy.data.scenes[0].cycles.volume_step_rate = 0.01
+
+
+try:
+	bpy.data.worlds["World"].node_tree.nodes.remove(bpy.data.worlds["World"].node_tree.nodes["Background"])
+except:
+	None
